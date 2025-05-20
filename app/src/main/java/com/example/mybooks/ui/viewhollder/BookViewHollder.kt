99e85ed979311c477353f95dd89a8d2b2d8eda96
Rafael Days <rafael.db.2003@gmail.com>
2025-05-20 17:32:22 -0300

    Recursos visuais do layout

--- app/src/main/java/com/example/mybooks/ui/viewhollder/BookViewHollder.kt
+++ app/src/main/java/com/example/mybooks/ui/viewhollder/BookViewHollder.kt
@@ -1,6 +1,7 @@
 package com.example.mybooks.ui.viewhollder
 
 import androidx.recyclerview.widget.RecyclerView
+import com.example.mybooks.R
 import com.example.mybooks.databinding.ItemBookBinding
 import com.example.mybooks.entity.BookEntity
 
@@ -8,5 +9,31 @@ class BookViewHollder(private val item:ItemBookBinding):RecyclerView.ViewHolder(
 
     fun bind(book: BookEntity){
         item.textviewTitle.text = book.titlle
+        item.textviewAuthor.text = book.author
+        item.textviewGenre.text = book.genre
+
+        updateFavoriteIcon(book.favorite)
+        setGenreBackground(book.genre)
+    }
+
+    private fun updateFavoriteIcon(favorite: Boolean){
+        if (favorite){
+            item.imageviewFavorite.setImageResource(R.drawable.ic_favorite)
+        } else
+            item.imageviewFavorite.setImageResource(R.drawable.ic_favorite_empty)
+    }
+
+    private fun setGenreBackground(genre: String){
+        when(genre) {
+            "Fantasia" -> {
+                item.textviewGenre.setBackgroundResource(R.drawable.rounded_label_fantasy)
+            }
+            "Terror" -> {
+                item.textviewGenre.setBackgroundResource(R.drawable.rounded_label_red)
+            }
+            else -> {
+                item.textviewGenre.setBackgroundResource(R.drawable.rounded_label_teal)
+            }
+        }
     }
 }
